86792e428da82c6c62247d76b585f5ae99852b74
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2019-07-02 17:17:28 +0100

    dracut: keep splitting parsing into more functions

--- testing/dracut-test.c
+++ testing/dracut-test.c
@@ -10,6 +10,44 @@
 #include <wicked/types.h>
 #include <wicked/util.h>
 
+/** unquoute: this is taken from sysconfig.c,
+ * find a place to put it
+ */
+static ni_bool_t
+unquote(char *string)
+{
+	char quote_sign = 0;
+	char *src, *dst, cc, lc = 0;
+	ni_bool_t ret = TRUE;
+
+	if (!string)
+		return ret;
+
+	ret = TRUE;
+	src = dst = string;
+	if (*string == '"' || *string == '\'') {
+		quote_sign = *string;
+		src++;
+	}
+	do {
+		cc = *src;
+		if (!cc) {
+			ret = quote_sign && lc == quote_sign;
+			break;
+		}
+		if (isspace(cc) && !quote_sign)
+			break;
+		if (cc == quote_sign)
+			break;
+		*dst = lc = cc;
+		dst++;
+		src++;
+	} while (1);
+
+	*dst = '\0';
+	return ret;
+}
+
 /**
  * Return a pointer to the first non-whitespace char in the line
  */
@@ -30,16 +68,35 @@ ni_dracut_cmdline_cleanup_line(ni_stringbuf_t *line)
 	return start;
 }
 
+/**
+ * Takes a line and extracts to a ni_string_array all the variables
+ */
+static char *
+ni_dracut_cmdline_from_line(ni_string_array_t *cmdline, const ni_string_array_t *lines)
+{
+	int i;
+	char *sp;
+
+	for (i = 0; i < lines->count; ++i) {
+		sp = lines->data[i];
+		printf("quote appears %d times\n", ni_string_count_char(lines->data[i], "\""));
+		/*`while (*sp != '\n' && *sp != '\0') {
+
+		}*/
+	}
+}
+
 static ni_bool_t
-ni_dracut_cmdline_from_file(ni_string_array_t *cmdlines, const char *filename)
+ni_dracut_cmdline_from_file(ni_string_array_t *lines, const char *filename)
 {
 	ni_stringbuf_t line = NI_STRINGBUF_INIT_DYNAMIC;
+	ni_string_array_t cmdline = NI_STRING_ARRAY_INIT;
 	char buf[BUFSIZ], eol;
 	char *start = NULL;
 	size_t len;
 	FILE *file;
 
-	if (!cmdlines || ni_string_empty(filename))
+	if (!lines || ni_string_empty(filename))
 		return FALSE;
 
 	if (!(file = fopen(filename, "r")))
@@ -54,8 +111,13 @@ ni_dracut_cmdline_from_file(ni_string_array_t *cmdlines, const char *filename)
 		if (len)
 			ni_stringbuf_puts(&line, buf);
 		if (eol) {
-			start = ni_dracut_cmdline_cleanup_line(&line);
-			ni_string_array_append(cmdlines, start);
+			//start = ni_dracut_cmdline_cleanup_line(&line);
+			//ni_string_array_append(lines, start);
+
+			// Alternative
+			ni_stringbuf_trim_head(&line, " ");
+			ni_string_array_append(lines, line.string);
+			ni_dracut_cmdline_from_line(&cmdline, lines);
 			ni_stringbuf_clear(&line);
 		}
 	}
@@ -67,26 +129,32 @@ ni_dracut_cmdline_from_file(ni_string_array_t *cmdlines, const char *filename)
 
 
 static ni_bool_t
-ni_dracut_cmdline_parse_ip(const char *value)
+ni_dracut_cmdline_parse_ip(char *value)
 {
 	return TRUE;
 }
 
+static char *
+ni_dracut_cmdline_cleanup(char *line) {
+	unquote(line);
+	return line;
+}
+
 static ni_bool_t
-ni_dracut_cmdline_parse(const ni_string_array_t *cmdlines)
+ni_dracut_cmdline_parse(const ni_string_array_t *lines)
 {
 	unsigned int i;
-	const char *value = NULL;
-	if (!cmdlines)
+	const char *value = NULL, *line = NULL;
+	if (!lines)
 		return FALSE;
 
-	for (i = 0; i < cmdlines->count; ++i) {
-		/*
-		line = cleanup_mess(cmdlines->data[i]);
-		*/
-		printf(">>>%s<<<\n", cmdlines->data[i]);
-		if (ni_string_startswith(cmdlines->data[i], "ip=")) {
-			value = ni_string_strip_prefix(cmdlines->data[i], "ip=");
+	for (i = 0; i < lines->count; ++i) {
+
+		line = ni_dracut_cmdline_cleanup(lines->data[i]);
+
+		printf(">>>%s<<<\n", lines->data[i]);
+		if (ni_string_startswith(lines->data[i], "ip=")) {
+			value = ni_string_strip_prefix("ip=", lines->data[i]);
 			printf("ip line value: %s \n", value);
 		}
 		/*
@@ -113,7 +181,7 @@ ni_dracut_cmdline_parse(const ni_string_array_t *cmdlines)
 int
 main(int argc, char *argv[])
 {
-	ni_string_array_t cmdlines = NI_STRING_ARRAY_INIT;
+	ni_string_array_t lines = NI_STRING_ARRAY_INIT;
 	ni_string_array_t files = NI_STRING_ARRAY_INIT;
 	const char *directory = "dracut";
 	unsigned int i;
@@ -123,15 +191,15 @@ main(int argc, char *argv[])
 		char *filename = NULL;
 
 		ni_string_printf(&filename, "%s/%s", directory, files.data[i]);
-		ni_dracut_cmdline_from_file(&cmdlines, filename);
+		ni_dracut_cmdline_from_file(&lines, filename);
 		ni_string_free(&filename);
 	}
 	ni_string_array_destroy(&files);
 	for (i = 1; i < (unsigned int)argc && argv[i]; ++i) {
-		ni_string_array_append(&cmdlines, argv[i]);
+		ni_string_array_append(&lines, argv[i]);
 	}
 
-	ni_dracut_cmdline_parse(&cmdlines);
-	ni_string_array_destroy(&cmdlines);
+	ni_dracut_cmdline_parse(&lines);
+	ni_string_array_destroy(&lines);
 	return 0;
 }
