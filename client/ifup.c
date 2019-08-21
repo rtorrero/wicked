82b5fd080dbd3c943829f282dc3946f9426db127
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2019-08-21 12:32:31 +0100

    wip: disregard commit (will be overwritten)

--- client/ifup.c
+++ client/ifup.c
@@ -38,6 +38,7 @@
 #include <wicked/netinfo.h>
 #include <wicked/logging.h>
 #include <wicked/fsm.h>
+#include <wicked/system.h>
 
 #include "client/ifconfig.h"
 
@@ -45,6 +46,9 @@
 #include "appconfig.h"
 #include "ifup.h"
 #include "ifstatus.h"
+#include "process.h"
+#include "main.h"
+#include "dhcp4/tester.h"
 
 struct ni_nanny_fsm_monitor {
 	const ni_timer_t *      timer;
@@ -986,195 +990,30 @@ ni_do_ifup(int argc, char **argv)
 int
 ni_do_bootstrap(int argc, char **argv)
 {
-	enum  { OPT_HELP, OPT_IFCONFIG, OPT_TIMEOUT, OPT_TRANSIENT };
+	ni_shellcmd_t *shellcmd;
+	ni_process_t *pi;
+	char *tester_argv[] = {"test", "dhcp4", "--output", "/tmp/lease.txt", "eth0"};
+	int tester_argc = 5;
+	int rv;
 
-	static struct option ifup_options[] = {
-		{ "help",	no_argument,       NULL,	OPT_HELP },
-		{ "ifconfig",	required_argument, NULL,	OPT_IFCONFIG },
-		{ "timeout",	required_argument, NULL,	OPT_TIMEOUT },
-		{ "transient", 	no_argument,		NULL,	OPT_TRANSIENT },
-		{ NULL }
-	};
+	shellcmd = ni_shellcmd_parse("wicked");
+	if (!shellcmd)
+		return -1;
 
-	ni_ifmatcher_t ifmatch;
-	ni_ifmarker_t ifmarker;
-	ni_ifworker_array_t ifmarked;
-	ni_string_array_t opt_ifconfig = NI_STRING_ARRAY_INIT;
-	ni_string_array_t ifnames = NI_STRING_ARRAY_INIT;
-	ni_bool_t check_prio = TRUE;
-	ni_bool_t opt_transient = FALSE;
-	unsigned int nmarked;
-	ni_fsm_t *fsm;
-	int c, status = NI_WICKED_RC_USAGE;
-	unsigned int timeout = 0;
-	const char *ptr;
+	pi = ni_process_new(shellcmd);
+	ni_shellcmd_free(shellcmd);
+	if (!pi)
+		return -1;
 
-	fsm = ni_fsm_new();
-	ni_assert(fsm);
-	ni_fsm_require_register_type("reachable", ni_ifworker_reachability_check_new);
+	pi->exec = do_test_dhcp4;
+	rv = ni_process_run(pi);
+	return 0;
+}
 
-	memset(&ifmatch, 0, sizeof(ifmatch));
-	memset(&ifmarker, 0, sizeof(ifmarker));
-	memset(&ifmarked, 0, sizeof(ifmarked));
-
-	/* Allow ifup on all interfaces we have config for */
-	ifmatch.require_configured = FALSE;
-	ifmatch.allow_persistent = TRUE;
-	ifmatch.require_config = TRUE;
-
-	ifmarker.target_range.min = __NI_FSM_STATE_MAX - 1;
-	ifmarker.target_range.max = __NI_FSM_STATE_MAX;
-
-	/*
-	 * Workaround to consider WAIT_FOR_INTERFACES variable
-	 * in network/config (bnc#863371, bnc#862530 timeouts).
-	 * Correct would be to get it from compat layer, but
-	 * the network/config is sourced in systemd service...
-	 */
-	if ((ptr = getenv("WAIT_FOR_INTERFACES"))) {
-		unsigned int sec;
-
-		if (ni_parse_uint(ptr, &sec, 10) == 0 &&
-		    (sec * 1000 > fsm->worker_timeout)) {
-			ni_debug_application("wait %u sec for interfaces", sec);
-			timeout = sec * 1000;
-		}
-	}
-
-	optind = 1;
-	while ((c = getopt_long(argc, argv, "", ifup_options, NULL)) != EOF) {
-		switch (c) {
-		case OPT_IFCONFIG:
-			ni_string_array_append(&opt_ifconfig, optarg);
-			break;
-
-		case OPT_TIMEOUT:
-			if (!strcmp(optarg, "infinite")) {
-				timeout = NI_IFWORKER_INFINITE_TIMEOUT;
-			} else if (ni_parse_uint(optarg, &timeout, 10) >= 0) {
-				timeout *= 1000; /* sec -> msec */
-			} else {
-				ni_error("ifup: cannot parse timeout option \"%s\"", optarg);
-				goto usage;
-			}
-			break;
-
-		case OPT_TRANSIENT:
-			opt_transient = TRUE;
-			break;
-
-		default:
-		case OPT_HELP:
-usage:
-			fprintf(stderr,
-				"wicked [options] bootstrap [ifup-options] <ifname ...>|all\n"
-				"\nSupported bootstrap-options:\n"
-				"  --help\n"
-				"      Show this help text.\n"
-				"  --transient\n"
-				"      Enable transient interface return codes\n"
-				"  --ifconfig <pathname>\n"
-				"      Read interface configuration(s) from file/directory rather than using system config\n"
-				"  --timeout <sec>\n"
-				"      Timeout after <sec> seconds\n"
-				);
-			goto cleanup;
-		}
-	}
-
-	if (optind >= argc) {
-		fprintf(stderr, "Missing interface argument\n");
-		goto usage;
-	}
-
-	if (!ni_fsm_create_client(fsm)) {
-		/* Severe error we always explicitly return */
-		status = NI_WICKED_RC_ERROR;
-		goto cleanup;
-	}
-
-	if (!ni_fsm_refresh_state(fsm)) {
-		/* Severe error we always explicitly return */
-		status = NI_WICKED_RC_ERROR;
-		goto cleanup;
-	}
-
-	if (opt_ifconfig.count == 0) {
-		const ni_string_array_t *sources = ni_config_sources("ifconfig");
-
-		if (sources && sources->count)
-			ni_string_array_copy(&opt_ifconfig, sources);
-
-		if (opt_ifconfig.count == 0) {
-			ni_error("ifup: unable to load interface config source list");
-			status = NI_WICKED_RC_NOT_CONFIGURED;
-			goto cleanup;
-		}
-	}
-
-	if (!ni_ifconfig_load(fsm, opt_global_rootdir, &opt_ifconfig, check_prio, TRUE)) {
-		status = NI_WICKED_RC_NOT_CONFIGURED;
-		goto cleanup;
-	}
-
-	if (ni_fsm_build_hierarchy(fsm, TRUE) < 0) {
-		ni_error("ifup: unable to build device hierarchy");
-		/* Severe error we always explicitly return */
-		status = NI_WICKED_RC_ERROR;
-		goto cleanup;
-	}
-
-	/* Get workers that match given criteria */
-	nmarked = 0;
-	while (optind < argc) {
-		ifmatch.name = argv[optind++];
-
-		if (!strcmp(ifmatch.name, "boot")) {
-			ifmatch.name = "all";
-			ifmatch.mode = "boot";
-		}
-
-		ni_fsm_get_matching_workers(fsm, &ifmatch, &ifmarked);
-
-		if (ni_string_eq(ifmatch.name, "all") ||
-		    ni_string_empty(ifmatch.name)) {
-			ni_string_array_destroy(&ifnames);
-			break;
-		}
-
-		if (ni_string_array_index(&ifnames, ifmatch.name) < 0)
-			ni_string_array_append(&ifnames, ifmatch.name);
-	}
-
-	ni_fsm_pull_in_children(&ifmarked, fsm);
-
-	/* Mark and start selected workers */
-	if (ifmarked.count)
-		nmarked = ni_fsm_mark_matching_workers(fsm, &ifmarked, &ifmarker);
-
-	if (nmarked == 0) {
-		ni_note("ifup: no matching interfaces");
-		status = NI_WICKED_RC_SUCCESS;
-	} else {
-		if (ni_fsm_schedule(fsm) != 0)
-			ni_fsm_mainloop(fsm);
-
-		ni_fsm_wait_tentative_addrs(fsm);
-
-		status = ni_ifstatus_display_result(fsm, &ifnames, &ifmarked,
-			opt_transient);
-
-		/*
-		 * Do not report any errors to systemd -- returning an error
-		 * here, will cause sytemd to stop the network completely.
-		 */
-		if (opt_systemd)
-			status = NI_LSB_RC_SUCCESS;
-	}
-
-cleanup:
-	ni_ifworker_array_destroy(&ifmarked);
-	ni_string_array_destroy(&ifnames);
-	ni_string_array_destroy(&opt_ifconfig);
-	return status;
+static int
+do_test_dhcp4(int argc, char *const argv[], char *const envp[])
+{
+	char *tester_argv[] = {"test", "dhcp4", "--output", "/tmp/lease.txt", "eth0"};
+	int tester_argc = 5;
+	ni_do_test_dhcp4("wicked", tester_argc, tester_argv);
 }
